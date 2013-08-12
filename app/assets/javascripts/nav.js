0f1fa0ab63d84f7a0c4fdc3e34f556b8cff3a5ae
David Ortiz <davidortizfrau@gmail.com>
2013-08-11 22:01:16 -0600

    Nav CSS

---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,5 @@
+$(function(){
+	$("a#resources").add("a#resources_active").text("Resources");
+	$("a#assignments").add("a#assignments_active").text("Project + Assignments");
+	$("a#syllabus").add("a#syllabus_active").text("Syllabus + Schedule");
+});
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,4 +5,8 @@ class Assignment < ActiveRecord::Base
 
   has_many :assignment_posts
   has_many :posts , through: :assignment_posts
+
+  default_scope order("due_date")
+
+  scope :next, where("due_date >= ?", Date.today).limit(4)
 end
