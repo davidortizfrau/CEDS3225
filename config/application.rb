956a66e6e4ae4902720488ab08cb9be90525d02f
David Ortiz <davidortizfrau@gmail.com>
2014-01-13 15:11:19 -0700

    precompiled assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,7 @@ module Ceds3225
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
   end
 end
